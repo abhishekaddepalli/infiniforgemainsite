2e6ecd1c25769015c33dc3ad9a9fad59fd2c0b6a
abhishekaddepalli <abhishekaddepalli1@gmail.com>
2026-07-17 12:21:31 +0530

    Add CommonJS wrapper for Passenger

---
+++ app.cjs
@@ -0,0 +1,11 @@
+async function startServer() {
+  try {
+    // Dynamically import the compiled ES Module server
+    await import('./.output/server/index.mjs');
+    console.log("Server loaded successfully via Passenger wrapper.");
+  } catch (err) {
+    console.error("Failed to load the server:", err);
+  }
+}
+
+startServer();
